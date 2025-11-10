10904c220619ad35a26a7e28bf3de14cad892848
Ansh Agnihotri <mailforwork.ansh1@gmail.com>
2025-11-10 12:45:27 +0530

    new commit

--- book app/new_book.jsx
+++ book app/new_book.jsx
@@ -14,5 +14,53 @@ function Change() {
     </div>
   );
 }
+return (
+  <div style = {{textAlign: "center", marginTop:"20px"}}>
+    <button onClick = {change}>Click</button>
+    <p>Hello clicked {count} times</p>
+  </div>
+)
 
-export default Change;
+export default Change;
+import React from "react";
+import './Book.css'
+
+function Book(){
+  return(
+    <div id="book">
+      <img src="" alt="" height={100} width={100}/>
+      <h1>TITLE:Maths</h1>
+      <h1>250</h1>
+
+      <img src="" alt="" height={100} width={100}/>
+      <h1>TITLE:English</h1>
+      <h1>250</h1>
+
+      <img src="" alt="" height={100} width={100}/>
+      <h1>TITLE:Science</h1>
+      <h1>250</h1>
+
+      <img src="" alt="" height={100} width={100}/>
+      <h1>TITLE:Hindi</h1>
+      <h1>250</h1>
+    </div>
+
+    function Link1() {
+      return (
+        <div>
+          <Routes>
+            <Route path = "/" element = {<home/>} />
+            <Route path = "/" element = {<home/>} />
+
+          </Routes>
+        </div>
+      )
+    }
+  )
+}
+// #book{
+//   border: 5px solid red;
+//   height: 300px;
+//   width: 300px;
+//   background-color:coral;
+// }class
